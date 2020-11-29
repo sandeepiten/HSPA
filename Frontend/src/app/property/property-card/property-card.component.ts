c508f560bede69f681542c5ebe2e2c3b9c3477e3
sandeepiten <sandeepiten@gmail.com>
2020-11-29 18:17:00 +0530

    first commit

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector : 'app-property-card',
+  templateUrl: 'property-card.component.html'
+
+}
+
+)
+export class PropertyCradComponent{
+
+
+}
